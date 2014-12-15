5654f3f6b1c1e9a620a105b2419b98656a0dcf16
Colin Ross <colin@colinscape.com>
2014-12-15 20:40:03 +0000

    Omit .git directory

--- upload.sh
+++ upload.sh
@@ -1,4 +1,4 @@
-rsync -v -a --exclude=node_modules --exclude=server.log --exclude=data --exclude=.npm . colin@lab.colinscape.com:~/site
+rsync -v -a --exclude=.git --exclude=node_modules --exclude=server.log --exclude=data --exclude=.npm . colin@lab.colinscape.com:~/site
 ssh colin@lab.colinscape.com "cd site && npm install"
 ssh colin@lab.colinscape.com "sudo killall node"
 ssh colin@lab.colinscape.com "cd site && ./start.sh"
