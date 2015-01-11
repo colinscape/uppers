3a8855802235d05b424879ebf1fe5b15983a889a
Colin Ross <colin@colinscape.com>
2015-01-11 15:12:30 +0000

    Automatic restarts

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-nohup sudo ./node_modules/coffee-script/bin/coffee server.coffee > ./server.log 2>&1 &
+nohup sudo ./node_modules/supervisor/lib/cli-wrapper.js --exec node_modules/coffee-script/bin/coffee server.coffee > ./server.log 2>&1 &
